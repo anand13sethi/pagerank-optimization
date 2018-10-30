1841ac815a438351d6204a4459171f8f27a761dd
Indraneeldas <indraneel.das@students.iiit.ac.in>
2018-10-30 19:08:17 +0000

    Bug fixes in vanilla pagerank

--- graph_generator.py
+++ graph_generator.py
@@ -1,11 +1,15 @@
 import random
 
+no_of_nodes = 1000
+no_of_edges = 499500
+
+
 with open("1000_nodes.txt", 'w') as fopen:
-    for i in range(0, 499500):
-        a = random.randint(1, 1000)
-        b = random.randint(1, 1000)
+    for i in range(0, no_of_edges):
+        a = random.randint(0, no_of_nodes-1)
+        b = random.randint(0, no_of_nodes-1)
         if a == b:
-            b += 1
+            b = (b+1)%no_of_nodes;
             fopen.write(str(a) + " " + str(b) + "\n")
         else:
             fopen.write(str(a) + " " + str(b) + "\n")
--- vanilla_pagerank.py
+++ vanilla_pagerank.py
@@ -1,7 +1,9 @@
-n = 1000
+n = 999
+init_rank = 1/(n+1)
 list_of_list = {}
-outdegree = [0]*1001
-rank = [1]*1001
+outdegree = [0]*(n+1)
+rank = [init_rank]*(n+1)
+new_rank = [0]*(n+1)
 with open("1000_nodes.txt", 'r') as fopen:
     for line in fopen:
         a, b = map(int, line.split())
@@ -16,13 +18,36 @@ with open("1000_nodes.txt", 'r') as fopen:
 #     for item in list_of_list:
 #         fout.write(item + " - " + " ".join(list_of_list[item]) + "\n")
 sum = 0
-d = 0.85
-for i in range(0, n+1):
-    # print(list_of_list[i])
-    if i in list_of_list.keys():
-        for item in list_of_list[i]:
-            sum += rank[item]/outdegree[item]
-    rank[i] = (1-d) + d*sum
-    sum = 0
+d = 1
+for j in range (0,10):
+    for i in range(0, n+1):
+        # print(list_of_list[i])
+        if i in list_of_list.keys():
+            # print(" i = " , i)
+            for item in list_of_list[i]:
+                # print ("item = " ,item,rank[item],outdegree[item])
+                donation = rank[item]/outdegree[item]
+                # print (donation)
+                sum += donation
+                # rank[item] -= donation
+        new_rank[i] = (1-d) + d*sum
+        # print("sum = ", sum);
+        sum = 0
+
+    for i in range(0,n+1):
+        rank[i] = new_rank[i]
+
+    # print (j)
+    # [print(i) for i in new_rank]
+
 
 [print(i) for i in rank]
+
+sum = 0
+
+for i in rank :
+    sum += i
+
+
+print ("sum =", sum)
+print(sum/(n+1))
